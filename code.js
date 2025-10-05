df9cb1adf7cdfe548db4f3c2fd479b7297a94169
khalilTaj <khalil.taj.aldean@gmail.com>
2025-10-05 21:14:53 +0300

    turned the day and time checker to a functions, leaving the old code in

--- code.js
+++ code.js
@@ -9,3 +9,14 @@ else{
     console.log("you cant post now");
 }
 ;
+
+function allowPosts(weekDay, time){
+    if (weekDay == 5 && time == 12){
+        return("u can post now")
+    }
+    else{
+        return("you cant post now")
+    };
+};
+
+console.log(allowPosts(Friday, Noon))
